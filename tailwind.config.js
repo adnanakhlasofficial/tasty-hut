3615fe5fcf046680edd3a61cdb7f07f4010db72d
adnanakhlasofficial <official.adnanakhlas@gmail.com>
2024-10-10 15:05:11 +0600

    Hero Done

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,10 @@ module.exports = {
       fontFamily: {
         poppins: "Poppins",
       },
+      backgroundImage: {
+        heroBg:
+          "linear-gradient(90deg, rgba(16,15,15,0.7147233893557423) 0%, rgba(16,15,15,0.711922268907563) 35%), url('../images/banner.webp')",
+      },
     },
   },
   plugins: [require("daisyui")],
